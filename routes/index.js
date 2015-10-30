2a6297e413fc9ac32cdac34a3216c8c5f8a37e25
Michael Snead <aikeru@gmail.com>
2015-10-29 23:54:12 -0400

    Use config for routes gallery

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var fs = require('fs'),
     path = require('path');
 
+var config = require('./config.json');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -10,7 +12,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/list-images', function(req, res, next) {
   //fs.readdir('./gallery/', function(err, files) {
-  fs.readdir('c:\\users\\aikeru\\downloads\\', function(err, files) {
+  fs.readdir(config.downloadFolder, function(err, files) {
     var result = files.filter(function(f) {
       return f.indexOf('jpg') > -1;
     });
@@ -23,7 +25,7 @@ router.get('/list-images', function(req, res, next) {
 
 router.get('/image/:fileName', function(req,res,next) {
   //var imageFile = path.join(require('path').dirname(require.main.filename), "..", "gallery", req.params.fileName);
-  var imageFile = path.join('c:\\users\\aikeru\\downloads\\' + req.params.fileName);
+  var imageFile = path.join(config.downloadFolder + req.params.fileName);
 console.log(imageFile);
   res.sendFile(imageFile);
 });
